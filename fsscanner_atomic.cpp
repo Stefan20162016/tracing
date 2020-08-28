3cf13f72ee4f61151f574cb6b224122da6a1ff7a
stefan20162016 <22373423+Stefan20162016@users.noreply.github.com>
2020-08-28 16:21:04 +0200

    2020_08_28

--- fsscanner_atomic.cpp
+++ fsscanner_atomic.cpp
@@ -2,11 +2,12 @@
 
 notes:
 
-    - g++ -O3 -std=c++17 -pthread main.cpp -o main && time sudo ./main 8 / # 1.3 sec
-    use -mno-omit-leaf-frame-pointer -fno-omit-frame-pointer for stack traces
-time ./main 8 /home/bm # 0.4sec
-threads are not synchonized or based on condition variable if work==more directories are available,
-just stealing from the global std::string vectors holding the directory names.
+ if (too many atomics): SLOW, else: same speed
+- g++ -O3 -std=c++17 -pthread fsscanner_atomic.cpp -o fsscanner_atomic && time sudo ./fsscanner_atomic 64 / 
+    
+
+
+threads are not synchonized or based on condition variables: just stealing from the global std::string vectors holding the directory names.
 Some threads might die early, and the last thread might scan the residual dirs.
 
     - Dcache scalability and RCU-walk: https://lwn.net/Articles/419811/
@@ -20,10 +21,13 @@ ulimit -n 123456
 # for more threads > 32k; I guess 2 mmap's per thread
 echo 777888 >  /proc/sys/vm/max_map_count
 
-e.g. use `iostat -x -p nvme0n1p1 -h 1` to find sweet spot
+e.g. use `iostat -x -p nvme0n1p1 --human 1` to find sweet spot
 mpstat -P ALL 1
 vmstat 1
 vmstat -m 1  | egrep "dentry|inode_cache" # print slabs
+
+bcc/tools/syscount.py to monitor syscalls
+
 
 read-ahead settings, scheduler, etc: https://cromwell-intl.com/open-source/performance-tuning/disks.html
 https://wiki.mikejung.biz/Ubuntu_Performance_Tuning
@@ -32,20 +36,21 @@ echo kyber > /sys/block/sda/queue/scheduler
 echo 512 > /sys/block/sda/queue/nr_requests
 echo 0 > /sys/block/sda/queue/read_ahead_kb
 
+echo kyber > /sys/block/nvme0n1/queue/scheduler
+echo 256 > /sys/block/nvme0n1/queue/nr_requests
+echo 0 > /sys/block/nvme0n1/queue/read_ahead_kb
+
 test filesystem on samsung nvme:
 128.000 directories
 1.350.000 files
 209GB
-time: 5 sec with 64-128 threads
+time: 5 sec with 128-200 threads on 4-core-i7
 */
-
 
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
-
-
 #include <locale>
 #include <chrono>
 #include <filesystem>
@@ -62,7 +67,10 @@ time: 5 sec with 64-128 threads
 #include <cstdlib>
 #include <dirent.h> 
 #include <sys/stat.h>
-
+#include <chrono>
+#include <typeinfo>
+#include <atomic>
+#include <ctime>
 //#define DEBUG 0
  
 thread_local std::string tls_path;
@@ -72,22 +80,32 @@ thread_local std::vector<std::string> tls_directories;
 //struct container{std::mutex mtx; std::vector<std::string> vec;};
 //std::array<container, 8> files_container;
 
-// note: eliminate false sharing
-#define NR_GLOBAL_FILENAME_MUTEXES 8
-std::array<std::mutex, NR_GLOBAL_FILENAME_MUTEXES> global_filename_mutexes;
+// note: eliminate false sharing?
+#define NR_GLOBAL_FILENAME_MUTEXES 8 // also works surprisingly well with 1
+std::array<std::mutex, NR_GLOBAL_FILENAME_MUTEXES> global_filename_mutexes; // mutexes for the following:
 std::array<std::vector<std::string>, NR_GLOBAL_FILENAME_MUTEXES> filename_array_of_vectors;
 
-#define NR_GLOBAL_DIRECTORIES_MUTEXES 16
-std::array<std::mutex, NR_GLOBAL_DIRECTORIES_MUTEXES> global_directories_mutexes;
+#define NR_GLOBAL_DIRECTORIES_MUTEXES 16 // also works surprisingly well with 1
+std::array<std::mutex, NR_GLOBAL_DIRECTORIES_MUTEXES> global_directories_mutexes; // mutexes for the following:
 std::array<std::vector<std::string>, NR_GLOBAL_DIRECTORIES_MUTEXES> directories_array_of_vectors;
 
 std::vector<std::string> global_filenames(0);
 std::vector<std::string> global_directories(0);
 std::vector<std::exception_ptr> global_exceptions;
-std::mutex coutmtx, global_exceptmutex;
+std::mutex coutmtx, global_exceptmutex; // mutex for std::cout and for exceptions thrown in threads
+
+#define NR_ATOMICS 8
+std::array<std::atomic<int>, NR_ATOMICS> atomic_running_threads;
 
 int global_residual_dirs=-1;
 int starting_dirs=-1;
+std::vector<uint64_t> running_times;
+std::mutex running_times_mutex;
+
+std::vector<uint64_t> starting_times;
+std::mutex starting_times_mutex;
+
+//std::chrono::time_point starting_time;
 
 class Worker {
 public:
@@ -97,22 +115,23 @@ public:
             if(start_with_path != ""){
                 tls_path=start_with_path;
                 do_linear_descent();
-                working();
+                #ifndef DONTSLEEP                
+                std::this_thread::sleep_for(std::chrono::milliseconds(1 + worker_id%8)); 
+                #endif  
+                working();  
             }
             else{
                 #ifdef DEBUG                
                 {std::lock_guard<std::mutex> lock(coutmtx);
                  std::cout << worker_id << ": EMPTY constructor string." << std::endl;}
                 #endif            
-
-                for(int i=0; i< 13; i++){    // try _ times to get some work
-                    #ifndef DONTSLEEP                
-                    std::this_thread::sleep_for(std::chrono::milliseconds(2 + worker_id%15)); 
-                    // helps to balance work for initially unemployed threads
-                    // check the filename_array_of_vectors[i].size() output at the end for even distribution
-                    #endif                    
-                    working(); 
-                }
+                //std::this_thread::sleep_for(std::chrono::milliseconds(4 + worker_id%8));
+                // ::sleep_for helps to balance work for initially unemployed threads
+                // check the filename_array_of_vectors[i].size() output at the end for even distribution and/or DIE_DEBUG
+                #ifndef DONTSLEEP                
+                std::this_thread::sleep_for(std::chrono::milliseconds(11 + worker_id%17));     
+                #endif                    
+                working(); 
             }
 
         } catch(...){
@@ -122,55 +141,131 @@ public:
     }
 
     void working(){
+        //std::chrono::time_point starting_time = std::chrono::high_resolution_clock::now();
+        auto starting_time = std::chrono::high_resolution_clock::now();
+        
+        uint64_t us = std::chrono::duration_cast<std::chrono::microseconds>(starting_time.time_since_epoch()).count();
+
+        //{
+        //std::lock_guard<std::mutex> lock(starting_times_mutex);
+        //std::lock_guard<std::mutex> lock2(coutmtx);
+        //
+        //std::cout << "worker: " << " time: " << us << std::endl;
+        //starting_times.push_back( us );
+        //}
+
         #ifdef DEBUG
         {std::lock_guard<std::mutex> lock(coutmtx);
         std::cout << worker_id << ": constructor string: " << tls_path << std::endl;}
         #endif
-
-    label_new:
+        #define RETRY_COUNT 1 // a few or a few dozen works equally well (for me)
+        int retry_count = RETRY_COUNT;
+    check_again:
         int proceed_new=0;
-        #define NR_OF_ROUNDS 2// a few or just one round works qually well
-        for(int i=0; proceed_new == 0 && i < NR_OF_ROUNDS*NR_GLOBAL_DIRECTORIES_MUTEXES; i++){ // 
+        #define NR_OF_ROUNDS 2 // a few or just one round works equally well
+        for(int i=0; proceed_new == 0 && i < NR_OF_ROUNDS*NR_GLOBAL_DIRECTORIES_MUTEXES; i++){  
             int start_with_i=(worker_id+i)%NR_GLOBAL_DIRECTORIES_MUTEXES; // start with own id-vector
             std::lock_guard<std::mutex> lock(global_directories_mutexes[start_with_i]);
             if(!directories_array_of_vectors[start_with_i].empty()){
                 tls_path=directories_array_of_vectors[start_with_i].back();
                 directories_array_of_vectors[start_with_i].pop_back();
                 proceed_new=1;
+                break;
             }
+            //std::this_thread::sleep_for(std::chrono::microseconds(1));
         }
-        if(proceed_new == 1){ // equal to if(tls_path != "") 
+        if(proceed_new == 1){ // equal to if(tls_path != "")
+            #ifdef DIE_DEBUG
+            if(retry_count != RETRY_COUNT){
+                std::lock_guard<std::mutex> lock(coutmtx);
+                std::cout << "worker: " << worker_id << " did reset RETRY_COUNT" << std::endl;
+            }
+            #endif
+            retry_count=RETRY_COUNT; // reset retry_count
             do_linear_descent();
         }
         else{
-            #ifdef NEWDEBUG
+            #ifdef DIE_DEBUG2
             {std::lock_guard<std::mutex> lock(coutmtx);
-             std::cout << worker_id << " is dying: did not find work in directories_array_of_vectors" << std::endl;
+             std::cout << "worker: " << worker_id << " is about to die at retry-lives-left:# " << retry_count << " did not find work in directories_array_of_vectors" << std::endl;
             }
             #endif
-            goto end_label_new; // thread will die here
+            goto end_label; // thread will die here eventually
         }
-        goto label_new; // check if there is more work
+        //std::this_thread::yield(); // does this help? it's at least faster than calling ::sleep_for()
 
-    end_label_new:  return;
+        goto check_again; // after returning from do_linear_descent() in if(proceed_new==1) above, check if there is more work
 
+    end_label:  
+        if(retry_count-- > 0){
+            std::this_thread::sleep_for(std::chrono::microseconds( 1251 ));
+            //std::this_thread::yield();
+            goto check_again;
+        }
+        else { 
+            #ifdef DIE_DEBUG
+             auto end_time = std::chrono::high_resolution_clock::now();
+             std::chrono::nanoseconds ns = end_time-starting_time;
+             auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(ns).count();
+             {std::lock_guard<std::mutex> lock(coutmtx);
+             std::cout << "worker: " << worker_id << " FINALLY DIED after: " << elapsed << " microseconds" << std::endl;
+             }
+            #endif
+
+            for(int i=0; i<NR_ATOMICS; i++){
+                if(atomic_running_threads[i] > 0){
+                    std::this_thread::sleep_for(std::chrono::microseconds(51));
+                    //std::this_thread::yield();
+                    goto check_again;
+                }
+            }
+            //
+            
+            //auto end_time = std::chrono::high_resolution_clock::now();
+            //std::chrono::nanoseconds ns = end_time-starting_time;
+            //auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(ns).count();
+            //{std::lock_guard<std::mutex> lock(coutmtx);
+            // std::cout << "worker: " << worker_id << " FINALLY DIED after: " << elapsed << " microseconds" << //std::endl;
+            //}
+
+            //{ std::lock_guard<std::mutex> lock(running_times_mutex);
+            //running_times.push_back( static_cast<uint64_t>(elapsed) );
+            //}
+            //
+            
+            if(false){
+                for(int i=0; i<NR_ATOMICS; i++){
+                    std::lock_guard<std::mutex> lock(coutmtx);
+                    if(atomic_running_threads[i] != 0){
+                        std::cout << "worker: " << worker_id << " atomic[" << i << "]= "<< atomic_running_threads[i] << std::endl;
+                    }
+                }
+            }
+
+            return; 
+        }
+                    
     }
 
 /*
  descent into ONE subdirectory found by `do_tree_walking()` linearly,
- meaning: step into ONE directory after another, till the end of the directory tree
+ meaning: step into ONE directory after another, till the end of this branch of the directory tree
 */
 
     void do_linear_descent() {
+
+        // increase atomic counter
+        atomic_running_threads[worker_id % NR_ATOMICS]++;
+        
         do {
             #ifdef DEBUG
             if(tls_path == ""){
                     std::lock_guard<std::mutex> lock(coutmtx);
                     std::cout << worker_id << ": ERROR tls_path " << tls_path << std::endl;
             }
-            #endif                    
+            #endif             
+
             do_dir_walking(tls_path);
-            //tls_path=""; // don't walk into same dir again
            
             // get one directory we found
             if (!tls_directories.empty()) {
@@ -185,7 +280,7 @@ public:
             else {
                 tls_path=""; // don't walk into same dir again
             }
-            // save filenames to global vector specific to this thread
+            // save filenames to global vector specific to this [thread_id % NR_MUTEXES]
             {
                 std::lock_guard<std::mutex> lock(global_filename_mutexes[worker_id%NR_GLOBAL_FILENAME_MUTEXES]);
                 for(auto const &v: tls_filenames){
@@ -204,6 +299,9 @@ public:
             tls_directories.clear();
 
         } while (tls_path != "");
+
+        // decrease atomic counter
+        atomic_running_threads[worker_id % NR_ATOMICS]--;
     }
 
 
@@ -255,9 +353,6 @@ int elf_hash(const char *filename){
     return hash;
 }
 
-
-
-
 int elf_hash_128(const char *filename){
     int hash=-1;
     //int closed=0;
@@ -296,11 +391,9 @@ int elf_hash_128(const char *filename){
     return hash;
 }
 
-
 private:
     const int worker_id = -272727;
     std::string start_with_path;
-
 
 int do_dir_walking(const std::string &sInputPath) {
     #ifdef DEBUG
@@ -326,18 +419,15 @@ int do_dir_walking(const std::string &sInputPath) {
                 {std::lock_guard<std::mutex> lock(coutmtx);
                 std::cout << "inode for file: " << sFileName << " is: " << inode << std::endl;}
                 #endif
-
                 #ifdef WITH_LSTAT   
                 if (lstat(sFileName.c_str(), &s_stat) == 0){
-                    if(s_stat.st_size >= 128){
-                        
+                    if(s_stat.st_size >= 128){  
                         int hash=elf_hash_128(sFileName.c_str()); // read 128 for every file with st_size >= 128 bytes
                         if(hash != -1){
                             tls_filenames.push_back(std::to_string(inode) + ";" + sFileName + ";" + std::to_string(hash));
                         }
                     }
                 }
-                
                 #endif
                 #ifndef WITH_LSTAT
                 int hash=elf_hash(sFileName.c_str()); // read 4 than 128 bytes if it's an ELF
@@ -363,7 +453,6 @@ int do_dir_walking(const std::string &sInputPath) {
 
 };
 
-
 void do_startup_file_walking(std::string starting_path){
     try {
         std::filesystem::path path(starting_path);
@@ -386,12 +475,8 @@ void do_startup_file_walking(std::string starting_path){
                     std::cout << "S file: ";
                     std::cout << path_entry << std::endl;
                     #endif                    
-           
-
-                    //global_filenames.emplace_back(path_entry);
                     Worker w(-42,"empty");
                     int hash=w.elf_hash(path_entry.c_str()); 
-                    
                     if(hash != -1){
                         struct stat s_stat;
                         if (lstat(path_entry.c_str(), &s_stat) == 0){
@@ -409,6 +494,10 @@ void do_startup_file_walking(std::string starting_path){
 }
 
 int main(int argc, char* argv[]) {
+    
+    //auto starting_time = std::chrono::high_resolution_clock::now();
+    //std::cout << typeid(starting_time).name() << std::endl;
+
     #ifdef WITH_LSTAT   
     std::cout << "Running with lstat(2) call" << std::endl;
     #endif
@@ -417,6 +506,12 @@ int main(int argc, char* argv[]) {
     #endif
     #ifdef PRINTFILENAMES
     std::cout << "Running with DPRINTFILENAMES" << std::endl;
+    #endif
+    #ifdef DIE_DEBUG
+    std::cout << "Running with DDIE_DEBUG" << std::endl;
+    #endif
+    #ifdef DIE_DEBUG2
+    std::cout << "Running with DDIE_DEBUG2" << std::endl;
     #endif
     
     int n_threads = -1;
@@ -502,10 +597,7 @@ int main(int argc, char* argv[]) {
     }
     #endif    
 
-
-
     // start threads(Worker)
-
     for(int i=0; i < n_threads; ++i){
         threads.emplace_back( std::ref(workers[i]) );
     }
@@ -518,7 +610,6 @@ int main(int argc, char* argv[]) {
     for(auto &v: threads){
         v.join();
     }
-
 
     /* process exceptions from threads */
     for(auto const &e: global_exceptions){
@@ -537,14 +628,21 @@ int main(int argc, char* argv[]) {
         std::cout << "global_directories is empty" << std::endl;
         #endif
     }
-  
-
     else {
         std::cout << "global_directories HAS MORE WORK => needs next round of threads!" << std::endl;
         for(auto const& v: global_directories){
             std::cout << "globdirs-residuals: " << v << std::endl;
         }
     }   
+
+    for(int i=0; i<NR_GLOBAL_DIRECTORIES_MUTEXES; i++){
+        if(!directories_array_of_vectors[i].empty()){
+            std::cout << "MORE WORK: directories_array_of_vectors[" << i << "].size(): " << directories_array_of_vectors[i].size() << std::endl;
+            for(auto const &v: directories_array_of_vectors[i]){
+                std::cout << "directories_array_of_vectors[" << i << "] = " << v << std::endl;
+            }
+        }
+    }
 
     // dont forget global_filenames:
     #ifdef DEBUG
@@ -591,6 +689,19 @@ int main(int argc, char* argv[]) {
     std::cout << "filename_array_of_vectors.size(): " << filename_array_of_vectors.size() << std::endl;
     std::cout << "number of files: file_sum: " << file_sum << std::endl;
     #endif
+
+    std::sort(running_times.begin(), running_times.end());
+    for(auto v: running_times){
+        std::cout << v << std::endl;
+    }
+    std::cout << "MAX_MIN:" << std::endl;
+    auto minmax = std::minmax_element(running_times.begin(), running_times.end());
+    // std::make_pair(first, first)
+    if(minmax != std::make_pair(running_times.begin(),running_times.begin() ) ){
+        std::cout << "max: " << *minmax.second << " min: " << *minmax.first << std::endl;
+    }
+    //auto dif = *minmax.second - *minmax.first;
+    //std::cout << "difference(microsecs) between max and min: " << dif << " millisecs: " << dif/1000 << std::endl;
 
 } // end int main(int argc, char* argv[])
 
